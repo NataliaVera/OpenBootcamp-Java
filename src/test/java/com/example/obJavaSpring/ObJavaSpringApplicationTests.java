75bc587a86848d168cc158fe0a91fe785bee6fab
Natalia Vera <nvera@coopebombas.com>
2022-09-05 13:12:26 -0500

    Primer commit

---
+++ src/test/java/com/example/obJavaSpring/ObJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
