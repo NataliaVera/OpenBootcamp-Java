9c80a5eaefd2bc614bf340a2dfc4f8e606d75ffb
Natalia Vera <nvera@coopebombas.com>
2022-09-07 12:31:38 -0500

    Clase 7

--- src/main/java/com/example/obJavaSpring/controller/BookController.java
+++ src/main/java/com/example/obJavaSpring/controller/BookController.java
@@ -2,6 +2,8 @@ package com.example.obJavaSpring.controller;
 
 import com.example.obJavaSpring.entity.Book;
 import com.example.obJavaSpring.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.Optional;
 //Devuelve Json
 @RestController
 public class BookController {
+
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     BookRepository repository;
@@ -42,14 +46,53 @@ public class BookController {
 
     //Crear un nuevo libro
     @PostMapping("/api/createBook")
-    public Book createBook(@RequestBody Book newBook, @RequestHeader HttpHeaders headers){
+    public ResponseEntity<Book> createBook(@RequestBody Book newBook, @RequestHeader HttpHeaders headers){
         System.out.println(headers.get("User-Agent"));
 
-        return repository.save(newBook);
+        if(newBook.getId() != null){
+            log.warn("trying to create a boook with id");
+            return ResponseEntity.badRequest().build();
+        }
+
+        Book result = repository.save(newBook);
+        return ResponseEntity.ok(result);
     }
 
     //actualizar un libro existente
+    @PutMapping ("/api/update")
+    public ResponseEntity<Book> updateBook(@RequestBody Book book){
+        Optional<Book> bookOptional = repository.findById(book.getId());
 
+        if(book.getId() == null){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.badRequest().build();
+        }
+
+        if(!repository.existsById(book.getId())){
+            log.warn("Trying to update a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+        Book result = repository.save(book);
+
+        return ResponseEntity.ok(result);
+    }
     // borrar un libro
+    @DeleteMapping("/api/delete/{id}")
+    public ResponseEntity<Book> delecte(@PathVariable Long id){
+        if(!repository.existsById(id)){
+            log.warn("Trying to delete a non existent book");
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(id);
 
+        return ResponseEntity.noContent().build();
+    }
+
+    //borrar todos
+    @DeleteMapping("/api/deleteAll")
+    public ResponseEntity<Book> deleteAll(){
+        log.info("REST Request for delete all books");
+        repository.deleteAll();
+        return ResponseEntity.noContent().build();
+    }
 }
